43a90dcd7bcd285d5943881366a9ff2bca7eb405
1 <386029724@qq.com>
2025-11-07 19:45:03 +0800

    fix clone

--- internel/utils/git.go
+++ internel/utils/git.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/osfs"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	ggssh "github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	"golang.org/x/crypto/ssh"
@@ -27,13 +28,14 @@ func NewSSHAuth(sshKeyPEM string) (*ggssh.PublicKeys, error) {
 	return auth, nil
 }
 
+// CloneOrUpdate 克隆或更新仓库到指定目录
 func CloneOrUpdate(baseDir, repoURL string, auth transport.AuthMethod) (*git.Repository, billy.Filesystem, error) {
 	// 用仓库地址计算 SHA256 作为文件夹名
 	hash := sha256.Sum256([]byte(repoURL))
 	folderName := hex.EncodeToString(hash[:])
 	repoDir := filepath.Join(baseDir, folderName)
 
-	fs := osfs.New(repoDir) // billy.Filesystem
+	fs := osfs.New(repoDir)
 
 	var repo *git.Repository
 	var err error
@@ -57,21 +59,46 @@ func CloneOrUpdate(baseDir, repoURL string, auth transport.AuthMethod) (*git.Rep
 			return nil, nil, fmt.Errorf("open existing repo: %w", err)
 		}
 
-		// 执行 pull 更新
+		// 先 fetch
+		err = repo.Fetch(&git.FetchOptions{
+			RemoteName: "origin",
+			Auth:       auth,
+			Force:      true,
+		})
+		if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+			return nil, nil, fmt.Errorf("fetch: %w", err)
+		}
+
+		// 找到远程默认分支
+		refs, err := repo.References()
+		if err != nil {
+			return nil, nil, fmt.Errorf("list references: %w", err)
+		}
+
+		var remoteRef *plumbing.Reference
+		err = refs.ForEach(func(r *plumbing.Reference) error {
+			if r.Name().IsRemote() && r.Name().String() != "" {
+				remoteRef = r
+				return errors.New("found") // 停止遍历
+			}
+			return nil
+		})
+		if remoteRef == nil {
+			return nil, nil, fmt.Errorf("no remote branch found")
+		}
+
+		// 强制重置本地工作区到远程最新
 		w, err := repo.Worktree()
 		if err != nil {
 			return nil, nil, fmt.Errorf("get worktree: %w", err)
 		}
 
-		pullOpts := &git.PullOptions{
-			RemoteName: "origin",
-			Auth:       auth,
-			Progress:   io.Discard,
-		}
-
-		// 如果已经是最新，会返回 git.NoErrAlreadyUpToDate，可以忽略
-		if err := w.Pull(pullOpts); err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
-			return nil, nil, fmt.Errorf("pull: %w", err)
+		err = w.Reset(&git.ResetOptions{
+			Mode:   git.HardReset,
+			Commit: remoteRef.Hash(),
+		})
+		if err != nil {
+			return nil, nil, fmt.Errorf("reset: %w", err)
 		}
 	}
 
